246f98f68b8f54422dc8b6b67b905295df881d57
Andreas Eberle <email@andreas-eberle.com>
2017-11-24 15:06:47 +0100

    Add comment to ECNRerankingForFeatures

--- evalECNRerankingForFeatures.m
+++ evalECNRerankingForFeatures.m
@@ -1,10 +1,11 @@
 function evalRerankingForFeatures(querymat, queryLab, queryCam, testmat, testLab, testCam)
-    noRerankingDist = pdist2(testmat, querymat, 'cosine');
+    noRerankingDist = pdist2(testmat, querymat, 'cosine'); % you can use euclidian here, if yor features are normalized
     [resultNoRerankingRanks, resultNoRerankingMap, ~, ~] = evaluation(noRerankingDist, testLab, queryLab, testCam, queryCam);
     
     origDistECN = ECN_rerank(querymat, testmat, 'method', 'origdist');
     [resultOrigDistECNRanks, resultOrigDistECNMap, ~, ~] = evaluation(origDistECN, testLab, queryLab, testCam, queryCam);
      
+	% This calculates the main result of ECN with rank distance (which is also default if method is not specified)
     rankDistECN = ECN_rerank(querymat, testmat, 'method', 'rankdist');
     [resultRankDistECNRanks, resultRankDistECNMap, ~, ~] = evaluation(rankDistECN, testLab, queryLab, testCam, queryCam);
     
